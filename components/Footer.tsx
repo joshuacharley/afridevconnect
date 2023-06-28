431f09714d669996b002205d46fc1c204133f26b
eatsalonedev <eatsalonedev@gmail.com>
2023-06-28 15:11:28 +0000

    added links for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,21 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+type ColumnProps = {
+    title: string
+    links: Array<string>;
+}
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+    <div className='footer_column'>
+        <h4 className='font-semibold'>{title}</h4>
+        <ul className='flex flex-col gap-2 font-normal'>
+            {links.map((link) => <Link href="/" key={link} >{link}</Link> )}
+        </ul>
+    </div>
+)
 
 const Footer = () => {
   return (
@@ -17,6 +33,11 @@ const Footer = () => {
                       can showcase their expertise in a form of project and also an app
                       where clients can hire skilled developers base on their project and level of experience.
                   </p>
+              </div>
+              <div className='flex flex-wrap gap-12'>
+                  <FooterColumn title={footerLinks[0].title}
+                   links={footerLinks[0].links} />
+                  
               </div>
               
           </div>
